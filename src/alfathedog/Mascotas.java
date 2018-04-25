491c1be9968b219dc0e19fda19f205a2de816169
Juan <Juan@DESKTOP-IU2B845>
2018-04-25 17:32:18 +0200

    jFrame mascotas

--- src/alfathedog/Mascotas.java
+++ src/alfathedog/Mascotas.java
@@ -11,12 +11,12 @@ import javax.swing.JFrame;
  *
  * @author steven
  */
-public class Clientes extends javax.swing.JFrame {
+public class Mascotas extends javax.swing.JFrame {
 
     /**
      * Creates new form Clientes
      */
-    public Clientes() {
+    public Mascotas() {
         initComponents();
         setSize(560, 430);
         jDialog1.setSize(560, 430);
@@ -108,7 +108,7 @@ public class Clientes extends javax.swing.JFrame {
 
     private void cerrarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cerrarSesionActionPerformed
         cerrarSesion.setVisible(true);
-         Clientes CL= new Clientes();
+         Mascotas CL= new Mascotas();
             
             CL.setVisible(false);
             
@@ -131,7 +131,7 @@ public class Clientes extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextBuscarClienteMouseClicked
 
     private void jButton1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MousePressed
-        Clientes CL= new Clientes();
+        Mascotas CL= new Mascotas();
         jDialog1.setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton1MousePressed
@@ -153,20 +153,21 @@ public class Clientes extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Clientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Mascotas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Clientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Mascotas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Clientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Mascotas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Clientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Mascotas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Clientes().setVisible(true);
+                new Mascotas().setVisible(true);
             }
         });
     }
